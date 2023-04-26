ac09412243f73a7ae82542b4e43f8b9f9240d41c
Arii95 <86978389+Arii95@users.noreply.github.com>
2023-04-26 10:09:48 -0600

    api

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from support_api import filter_area_perimetro, data_devices, select_data_by_date,setle_clean,df_gps,select_data_by_dates,agregar_ith
+from support_api import filter_area_perimetro, data_devices, select_data_by_date,setle_clean,df_gps,select_data_by_dates
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from starlette.responses import RedirectResponse
-import json
 from ml_suport import predict_model
 from aguadas import agua_click,result_select,agua_clicks
 import pandas as pd
+import json
 from transform_data import add_dormida_column,separador_por_dia, dataframe_interview_vaca,data_interview,diagnostico_devices
 #Creo una instancia de FastAPI
 app = FastAPI()
@@ -25,7 +25,7 @@ async def informacion_por_finca(nombre: str):
     merge_data = data_interview(nombre)
     merge_data.point_ini= merge_data.point_ini.astype(str)
     merge_data.point_next= merge_data.point_next.astype(str)
-    return JSONResponse(content= merge_data.to_dict('records'))
+    return JSONResponse(content= json.dumps(merge_data.to_dict('records')))
 
 
 # Segunda consulta: Informacion propia de una finca en un periodo de tienpo.
@@ -37,7 +37,7 @@ async def informacion_por_un_periodo_por_finca(nombre : str, fecha_init: str, fe
     df_gp = data_interview(nombre,df_gp)
     df_gp.point_ini= df_gp.point_ini.astype(str)
     df_gp.point_next= df_gp.point_next.astype(str)
-    return JSONResponse(content= df_gp.to_dict('records'))
+    return JSONResponse(content= json.dumps(df_gp.to_dict('records')))
 
 
 
@@ -48,7 +48,7 @@ async def filtro_por_una_vaca_establecimiento(nombre : str, id : str):
     df_gps = data_finca[data_finca.UUID==id]
     df_gps.point_ini= df_gps.point_ini.astype(str)
     df_gps.point_next= df_gps.point_next.astype(str)
-    return JSONResponse(content= df_gps.to_dict('records'))
+    return JSONResponse(content= json.dumps( df_gps.to_dict('records')))
 
 
 
@@ -62,7 +62,7 @@ async def informacion_por_un_dia_una_vaca_por_finca(nombre : str, id : str, fech
     df_gp = dataframe_interview_vaca(df_gp)
     df_gp.point_ini= df_gp.point_ini.astype(str)
     df_gp.point_next= df_gp.point_next.astype(str)
-    return JSONResponse(content= df_gp.to_dict('records'))
+    return JSONResponse(content=  json.dumps(df_gp.to_dict('records')))
 
 
 # Quinta consulta: Toda la informacion de una vaca, en un establecimiento en un periodo de tiempo
@@ -75,7 +75,7 @@ async def informacion_por_un_periodo_una_vaca_por_finca(nombre : str, id : str, 
     df_gp = dataframe_interview_vaca(df_gp)
     df_gp.point_ini= df_gp.point_ini.astype(str)
     df_gp.point_next= df_gp.point_next.astype(str)
-    return JSONResponse(content= df_gp.to_dict('records'))
+    return JSONResponse(content= json.dumps(df_gp.to_dict('records')))
 
 # Sexta consulta:
 @app.get("/conducta_vaca/{nombre}/{id}/{fecha}")
@@ -85,7 +85,6 @@ async def conducta_vaca(nombre : str, id : str, fecha: str):
     df_gp= data_devices(finca,id)
     df_gp = select_data_by_date(df_gp,fecha)
     df_gp = dataframe_interview_vaca(df_gp)
-    df_gp = agregar_ith(df_gp, fecha,str(data_finca._id.values[0]))
     df_gp = predict_model(df_gp)
     d = agua_click(finca, id ,fecha ,str(data_finca._id.values[0]))
     df_gp =result_select(df_gp,d)
@@ -108,7 +107,6 @@ async  def conducta_vaca_periodo(nombre : str, id : str, fecha_init: str, fecha_
     df_gp = data_devices(finca,id)
     df_gp = select_data_by_dates(df_gp,fecha_init,fecha_fin)
     df_gp = dataframe_interview_vaca(df_gp)
-    df_gp = agregar_ith(df_gp, fecha_init,str(data_finca._id.values[0]),fecha_fin)
     df_gp = predict_model(df_gp)
     d = agua_clicks(finca,id,fecha_init,fecha_fin,str(data_finca._id.values[0]))
     df_gp = result_select(df_gp,d)
